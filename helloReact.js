957b5a0124bd625e93a499f4144daf08f1445cc7
Adrian Shepelavey <adrianshepelavey@gmail.com>
2017-06-06 12:37:12 -0400

    1st exercise in learnyoureact

--- helloReact.js
+++ helloReact.js
@@ -1,6 +1,45 @@
 
 /*
 
+Here's the official solution in case you want to compare notes:
+
+────────────────────────────────────────────────────────────────────────────────
+solution.js:
+
+    var express = require('express');
+    var app = express();
+
+    app.set('port', (process.argv[2] || 3000));
+    app.set('view engine', 'jsx');
+    app.set('views', __dirname + '/views');
+    app.engine('jsx', require('express-react-views').createEngine({transformViews: false}));
+
+    require('babel/register')({
+        ignore: false
+    });
+
+    app.use('/', function (req, res) {
+        res.render('index', '');
+    });
+
+    app.listen(app.get('port'), function () {
+    });
+
+────────────────────────────────────────────────────────────────────────────────
+index.jsx:
+
+    import React from 'react';
+
+    export default class TodoBox extends React.Component{
+      render() {
+        return <div className="todoBox">
+            Hello, world!
+          </div>
+      }
+    }
+
+
+
 HELLO_REACT
  Exercise 1 of 11
 
