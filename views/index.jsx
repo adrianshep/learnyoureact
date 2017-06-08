f0875740820ad5a8c3d74d60693c5ea58d614a4a
Adrian Shepelavey <adrianshepelavey@gmail.com>
2017-06-08 09:31:37 -0400

    3rd exercise in learnyoureact

--- views/index.jsx
+++ views/index.jsx
@@ -1,33 +1,28 @@
 import React from 'react';
 
 export default class TodoBox extends React.Component {
-     render() {
-         return (
-             <div className="todoBox">
-                 <h1>Todos</h1>
-                 <TodoList />
-                 <TodoForm />
-             </div>
-         );
-     }
- }
+  // Omitted
+}
 
 class TodoList extends React.Component {
-   render() {
-    return (
-      <div className="todoList">
-        I am a TodoList.
-      </div>
-    );
-   }
- }
+    render() {
+        return (
+            <div className="todoList">
+                <table style={{border: "2px solid black"}}>
+                    <tbody>
+                    <Todo title="Shopping">Milk</Todo>
+                    <Todo title="Hair cut">13:00</Todo>
+                    </tbody>
+                </table>
+            </div>
+        );
+    }
+}
+
+class Todo extends React.Component {
+  // Write code here
+}
 
 class TodoForm extends React.Component {
-   render() {
-    return (
-      <div className="todoForm">
-        I am a TodoForm.
-      </div>
-    );
-   }
- }
+  // Omitted
+}
