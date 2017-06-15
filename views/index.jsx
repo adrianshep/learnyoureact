4f0a96136dd493d35e37523159d6aeb2c92c129b
Adrian Shepelavey <adrianshepelavey@gmail.com>
2017-06-15 09:27:25 -0400

    5th exercise in learnyoureact

--- views/index.jsx
+++ views/index.jsx
@@ -1,37 +1,24 @@
 import React from 'react';
 
 export default class TodoBox extends React.Component {
-    render() {
-        return (
-            <div className="todoBox">
-                <h1>Todos</h1>
-                <TodoList />
-                <TodoForm />
-            </div>
-        );
-    }
+  // Omitted
 }
 
 class TodoList extends React.Component {
-    render() {
-        return (
-            <div className="todoList">
-                <table style={{border: "2px solid black"}}>
-                  <tbody>
-                    <Todo title="Shopping">Milk</Todo>
-                    <Todo title="Hair cut">13:00</Todo>
-                    <Todo title="Learn React">15:00</Todo>
-                  </tbody>
-                </table>
-            </div>
-        );
-    }
+  // Omitted
 }
 
 class Todo extends React.Component {
+    constructor(props) {
+        super(props);
+    }
+
     render() {
         return (
             <tr>
+                <td style={{border: "1px solid black"}}>
+                    <input type="checkbox" checked={this.state.checked} onChange={this.handleChange.bind(this)}/>
+                </td>
                 <td style={{border: "1px solid black"}}>{this.props.title}</td>
                 <td style={{border: "1px solid black"}}>{this.props.children}</td>
             </tr>
@@ -43,11 +30,5 @@ Todo.propTypes = {
 };
 
 class TodoForm extends React.Component {
-    render() {
-        return (
-            <div className="todoForm">
-                I am a TodoForm.
-            </div>
-        );
-    }
+  // Omitted
 }
