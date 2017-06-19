0b3d1f12efc6f49d6d4348d5b83fcc4c2b696b8c
Adrian Shepelavey <adrianshepelavey@gmail.com>
2017-06-19 09:29:57 -0400

    3rd exercise in learnyoureact

--- props.js
+++ props.js
@@ -154,4 +154,27 @@ make sure it looks right.
 
 Once you're confident, run learnyoureact verify program.js.
 
+From codecademy:
+
+Every component has something called props.
+
+A component's props is an object. It holds information about that component.
+
+To see a component's props object, you use the expression this.props. Here's an
+example of this.props being used inside of a render function:
+
+render: function () {
+
+  console.log("Props object comin' up!");
+
+  console.log(this.props);
+
+  console.log("That was my props object!");
+
+  return <h1>Hello world</h1>;
+}
+
+Most of the information in this.props is pretty useless! But some of it is
+extremely important.
+
 */
