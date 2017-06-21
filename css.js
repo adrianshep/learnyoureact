925f63313e9654fdfece4c533e9af1df7978a5c5
Adrian Shepelavey <adrianshepelavey@gmail.com>
2017-06-21 12:58:51 -0400

    6th exercise in learnyoureact

--- css.js
+++ css.js
@@ -83,4 +83,26 @@ visiting http://localhost:3000 in your browser.
 
 Once you're confident, run learnyoureact verify program.js.
 
+From codecademy:
+
+An alternative that's often nicer is to store a style object in a variable, and
+then inject that variable into JSX.
+
+Look in the code editor for an example. The style object is defined on lines 3-6,
+and then injected on line 11.
+
+If you aren't used to using modules, then this code may have made you twitch
+uncontrollably:
+
+var style = {
+  color: 'darkcyan',
+  background: 'mintcream'
+};
+Defining a variable named style in the top-level scope would be an extremely bad
+idea in many JavaScript environments! In React, however, it's totally fine.
+
+Remember that every file is invisible to every other file, except for what you
+choose to expose via module.exports. You could have 100 different files, all
+with global variables named style, and there could be no conflicts.
+
 */
