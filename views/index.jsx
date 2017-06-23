00813e7613c24384a8e8b4c28bf3fb3857d6cbca
Adrian Shepelavey <adrianshepelavey@gmail.com>
2017-06-23 09:29:26 -0400

    5th exercise in learnyoureact

--- views/index.jsx
+++ views/index.jsx
@@ -31,6 +31,11 @@ class TodoList extends React.Component {
 class Todo extends React.Component {
     constructor(props) {
         super(props);
+        this.state = {checked: false};
+    }
+
+    handleChange(e) {
+    this.setState({checked: e.target.checked});
     }
 
     render() {
