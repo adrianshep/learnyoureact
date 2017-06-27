781bc5ea0aa4a02cbbc04e1650107ef35997cebf
Adrian Shepelavey <adrianshepelavey@gmail.com>
2017-06-27 09:33:49 -0400

    7th exercise in learnyoureact

--- program.js
+++ program.js
@@ -15,3 +15,28 @@ app.use('/', function(req, res) {
 });
 
 app.listen(app.get('port'), function() {});
+
+/*
+
+var express = require('express');
+var app = express();
+
+app.set('port', (process.argv[2] || 3000));
+app.set('view engine', 'jsx');
+app.set('views', __dirname + '/views');
+app.engine('jsx', require('express-react-views').createEngine({ transformViews: false }));
+
+require('babel/register')({
+    ignore: false
+});
+
+// write below
+var data = [];
+
+app.use('/', function(req, res) {
+  res.render('index', {data: data});
+});
+
+app.listen(app.get('port'), function() {});
+
+*/
