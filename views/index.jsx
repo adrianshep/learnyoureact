4a6352668a99a2de819fb42bc00d341417702169
Adrian Shepelavey <adrianshepelavey@gmail.com>
2017-06-29 11:58:59 -0400

    7th exercise in learnyoureact

--- views/index.jsx
+++ views/index.jsx
@@ -5,7 +5,7 @@ export default class TodoBox extends React.Component {
         return (
             <div className="todoBox">
                 <h1>Todos</h1>
-                <TodoList />
+                <TodoList data={this.props.data}/>
                 <TodoForm />
             </div>
         );
@@ -14,13 +14,14 @@ export default class TodoBox extends React.Component {
 
 class TodoList extends React.Component {
     render() {
+      let todo= this.props.data.map(function (obj) {
+          return <Todo title={obj.title} key={obj.title}>{obj.detail}</Todo>
+      });
         return (
             <div className="todoList">
               <table style={{border: "2px solid black"}}>
                 <tbody>
-                  <Todo title="Shopping">Milk</Todo>
-                  <Todo title="Hair cut">13:00</Todo>
-                  <Todo title="Learn React">15:00</Todo>
+                  {todo}
                 </tbody>
               </table>
             </div>
@@ -41,11 +42,10 @@ class Todo extends React.Component {
     render() {
         return (
             <tr>
-                <td style={{border: "1px solid black"}}>
-                    <input type="checkbox" checked={this.state.checked} onChange={this.handleChange.bind(this)}/>
-                </td>
-                <td style={{border: "1px solid black"}}>{this.props.title}</td>
-                <td style={{border: "1px solid black"}}>{this.props.children}</td>
+            <td style={style.tableContent}><input type="checkbox" checked={this.state.checked}
+                                                  onChange={this.handleChange}/></td>
+            <td style={style.tableContent}>{this.props.title}</td>
+            <td style={style.tableContent}>{this.props.children}</td>
             </tr>
         );
     }
