f8e012f8366be378e24f96843ac123156bd355e0
Adrian Shepelavey <adrianshepelavey@gmail.com>
2017-07-02 09:50:01 -0400

    8th exercise in learnyoureact

--- program.js
+++ program.js
@@ -1,4 +1,12 @@
+var React = require('react');
+var ReactDOMServer = require('react-dom/server');
+var DOM = React.DOM;
+var body = DOM.body;
+var div = DOM.div;
+var script = DOM.script;
 
+var browserify = require('browserify');
+var babelify = require("babelify");
 
 /*
 
