5ab89c3a9f4a6641c76f770b2cd528e9d0c86968
Adrian Shepelavey <adrianshepelavey@gmail.com>
2017-07-07 10:53:43 -0400

    8th exercise in learnyoureact

--- views/index.jsx
+++ views/index.jsx
@@ -5,70 +5,20 @@ export default class TodoBox extends React.Component {
         return (
             <div className="todoBox">
                 <h1>Todos</h1>
-                <TodoList data = {this.props.data} />
+                <TodoList data={this.props.data}/>
                 <TodoForm />
             </div>
         );
     }
 }
 
-module.exports = TodoBox;
-
 class TodoList extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            data: this.props.data,
-            titleValue: "",
-            detailValue: ""
-        };
-        this.changeTitle = this.changeTitle.bind(this);
-        this.changeDetail = this.changeDetail.bind(this);
-        this.addTodo = this.addTodo.bind(this);
-        this.deleteTodo = this.deleteTodo.bind(this);
-    }
-
-    changeTitle(e) {
-        this.setState({titleValue: e.target.value})
-    }
-
-    changeDetail(e) {
-        this.setState({detailValue: e.target.value})
-    }
-
-    addTodo() {
-        let data = this.state.data
-        data.push({
-            title: this.state.titleValue,
-            detail: this.state.detailValue
-        })
-        this.setState({
-            data: data,
-            titleValue: "",
-            detailValue: ""
-        })
-    }
-
-    deleteTodo(title) {
-        let data = this.state.data.filter(function(e){
-            return e.title != title
-        })
-        this.setState({
-            data: data
-        })
-    }
-
     render() {
-        let todo = this.state.data.map(function(obj) {
-            return <Todo title={obj.title} key={obj.title} onDelete={this.deleteTodo}>{obj.detail}</Todo>;
-        }.bind(this));
+        var todo = this.props.data.map(function (obj) {
+            return <Todo title={obj.title} key={obj.title}>{obj.detail}</Todo>
+        });
         return (
-            <div className = "todoList">
-                <div>
-                Title:<input type="text" value={this.state.titleValue} onChange={this.changeTitle} />
-                Detail:<input type="text" value={this.state.detailValue} onChange={this.changeDetail} />
-                <button onClick={this.addTodo}>Add</button>
-                </div>
+            <div className="todoList">
                 <table style={{border: "2px solid black"}}>
                     <tbody>
                     {todo}
@@ -82,30 +32,19 @@ class TodoList extends React.Component {
 class Todo extends React.Component {
     constructor(props) {
         super(props);
-        this.state = {
-          checked: false,
-          TodoStyle: style.notCheckedTodo
-        };
+        this.state = {checked: false};
         this.handleChange = this.handleChange.bind(this);
-        this._onDelete = this._onDelete.bind(this);
     }
 
     handleChange(e) {
-        this.setState({
-            checked: e.target.checked
-        })
-    }
-
-    _onDelete() {
-        this.props.onDelete(this.props.title);
+        this.setState({checked: e.target.checked});
     }
 
     render() {
         return (
-            <tr style={this.state.checked ? style.checkedTodo : style.notCheckedTodo}>
-                <td style={style.tableContent}><button onClick={this._onDelete}>X</button></td>
+            <tr>
                 <td style={style.tableContent}>
-                    <input type="checkbox" checked={this.state.checked} onChange={this.handleChange.bind(this)}/>
+                    <input type="checkbox" checked={this.state.checked} onChange={this.handleChange}/>
                 </td>
                 <td style={style.tableContent}>{this.props.title}</td>
                 <td style={style.tableContent}>{this.props.children}</td>
@@ -121,19 +60,13 @@ class TodoForm extends React.Component {
     render() {
         return (
             <div className="todoForm">
-              I am a TodoForm.
+                I am a TodoForm.
             </div>
         );
     }
 }
 
 let style = {
-    checkedTodo: {
-        textDecoration: "line-through"
-    },
-    notCheckedTodo: {
-        textDecoration: "none"
-    },
     tableContent: {
         border: "1px solid black"
     }
